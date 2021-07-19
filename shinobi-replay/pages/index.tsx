0082905e3fbc488003d7a8b52f99b3f363054fb9
Ross McMillan <rossmcmillan92@gmail.com>
2021-07-19 12:54:22 +0100

    about done.

--- shinobi-replay/pages/index.tsx
+++ shinobi-replay/pages/index.tsx
@@ -1,3 +1,23 @@
+import { readdir } from "fs"
+import { GetServerSideProps } from "next"
+import path from "path"
+
 const IndexRoute = () => <>Hello!</>
+
+const readDirectory = (dirPath: string) =>
+  new Promise((resolve, reject) =>
+    readdir(path.resolve(process.cwd(), dirPath), {}, (error, files) =>
+      error ? reject(error) : resolve(files)
+    )
+  )
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const videoFolder = await readDirectory("./out")
+  console.log(
+    "🚀 ~ file: index.tsx ~ line 9 ~ constgetServerSideProps:GetServerSideProps= ~ videoFolder",
+    videoFolder
+  )
+  return { props: {} }
+}
 
 export default IndexRoute
