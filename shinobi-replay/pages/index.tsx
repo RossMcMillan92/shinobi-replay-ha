ceae8231e486a35cf2ef587a277d4b2fc5210362
Ross McMillan <rossmcmillan92@gmail.com>
2021-07-20 09:47:26 +0100

    about done.

--- shinobi-replay/pages/index.tsx
+++ shinobi-replay/pages/index.tsx
@@ -1,16 +1,11 @@
 import { GetServerSideProps } from "next"
+import React from "react"
 import { readDirectory } from "../utils/fs"
 
 const IndexRoute = ({ videos }: { videos: string[] }) => (
   <>
     {videos.length > 0
-      ? videos.map((video) => (
-          <p>
-            <a target="_blank" href={`/out/${video}`}>
-              {video}
-            </a>
-          </p>
-        ))
+      ? videos.map((video) => <VideoItem video={video} />)
       : "No videos recorded yet!"}
   </>
 )
@@ -21,3 +16,29 @@ export const getServerSideProps: GetServerSideProps = async () => {
 }
 
 export default IndexRoute
+
+function VideoItem({ video }: { video: string }): JSX.Element {
+  console.log("🚀 ~ file: index.tsx ~ line 21 ~ VideoItem ~ video", video)
+  const [status, setStatus] = React.useState<"idle" | "open">("idle")
+  return (
+    <p>
+      <button
+        onClick={() =>
+          setStatus((status) => (status === "idle" ? "open" : "idle"))
+        }
+      >
+        {video}
+      </button>
+
+      {status === "open" ? (
+        <>
+          <video
+            style={{ width: "100%" }}
+            src={`/api/get-replay/${video}`}
+            controls={true}
+          ></video>
+        </>
+      ) : null}
+    </p>
+  )
+}
